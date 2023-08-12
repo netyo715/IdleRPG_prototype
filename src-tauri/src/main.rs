0d33aa2611b9d323b967496ed1a3afe779f5e681
sohei nishimura <nishimura715@gmail.com>
2023-08-13 02:55:55 +0900

    更新処理

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,71 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use tauri::{State, Manager};
+use std::sync::Mutex;
+
+struct MyCharacterState{
+    spd: usize,
+}
+
+struct BattleState{
+    is_valid: Mutex<bool>,
+}
+
+impl BattleState{
+    fn new(is_valid: bool) -> Self {
+        Self {
+            is_valid: Mutex::new(is_valid)
+        }
+    }
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn next_turn(my_character_state: State<MyCharacterState>) -> Result<usize, String>{
+    Ok(my_character_state.spd)
+}
+
+#[tauri::command]
+fn is_valid(state: State<BattleState>) -> Result<bool, String>{
+    let mut is_valid = state.is_valid.lock().unwrap();
+    Ok(*is_valid)
+}
+
+#[tauri::command]
+fn init_battle(state: State<BattleState>) -> Result<(), String>{
+    let mut is_valid = state.is_valid.lock().unwrap();
+    *is_valid = true;
+    Ok(())
+}
+
+#[tauri::command]
+fn close_battle(state: State<BattleState>) -> Result<(), String>{
+    let mut is_valid = state.is_valid.lock().unwrap();
+    *is_valid = false;
+    Ok(())
+}
+
+#[tauri::command]
+fn battle_action() -> Result<(), String>{
+    Ok(())
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app|{
+            let my_character_state = MyCharacterState{spd:1000};
+            app.manage(my_character_state);
+            let battle_state = BattleState::new(false);
+            app.manage(battle_state);
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            next_turn,
+            battle_action,
+            is_valid,
+            init_battle,
+            close_battle,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Battle/battle.tsx
+++ src/components/Battle/battle.tsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import {tauri} from '@tauri-apps/api';
+
+const getNextTurn = async () => {
+    return await tauri.invoke<number>("next_turn");
+}
+
+const initBattle = async () => {
+    await tauri.invoke("init_battle");
+}
+
+const closeBattle = async () => {
+    console.log("close");
+    await tauri.invoke("close_battle");
+}
+
+const isValid = async () => {
+    return await tauri.invoke<boolean>("is_valid");
+}
+
+const battleAction = async () => {
+    isValid().then(is_valid => {
+        if (is_valid){
+            console.log("battle");
+            getNextTurn().then(interval => {
+                setTimeout(battleAction, interval);
+            });
+        }
+    });
+}
+
+const startBattle = async () => {
+    console.log("start");
+    isValid().then(is_valid => {
+        if (!is_valid){
+            initBattle()
+            console.log("init")
+            getNextTurn().then(interval => {
+                setTimeout(battleAction, interval);
+            });
+        }
+    });
+}
 
 const Battle: React.FC = () => {
     return (
-        <div style={{height: "100%", flex: "1", background: "lightpink"}}></div>
+        <div style={{height: "100%", flex: "1", background: "lightpink"}}>
+            <button onClick={startBattle}>スタート</button>
+            <button onClick={closeBattle}>終了</button>
+        </div>
     )
 }
  
